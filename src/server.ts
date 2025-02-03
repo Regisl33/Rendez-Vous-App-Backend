d33794ffc3cf4ab7ad0493ddda50673dfb2573f3
Regisl33 <reg33gaming@gmail.com>
2025-02-02 22:18:45 -0500

    added cors config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import dotenv from "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from "./config/dbConnect";
+import cors from "cors";
+import corsOptions from "./config/corsOptions";
 
 const app = express();
 const PORT = process.env.PORT || "3500";
 
 connectDB();
+
+app.use(cors(corsOptions));
 
 mongoose.connection.once("open", () => {
   app.listen(PORT, () => console.log("Server running on port:" + PORT));
