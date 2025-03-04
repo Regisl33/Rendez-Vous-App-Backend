1af852609469434c87270daf25a63d0f9e87a303
Regisl33 <reg33gaming@gmail.com>
2025-03-03 19:52:38 -0500

    added comments

--- src/routes/serviceRoute.ts
+++ src/routes/serviceRoute.ts
@@ -3,10 +3,10 @@ import * as ServiceControllers from "../controllers/servicesController";
 
 const router = express.Router();
 
-router.route("/").get(ServiceControllers.getServices);
-router.route("/:currentStoreID").get(ServiceControllers.getServicesByStore);
-router.route("/modif/:serviceID").get(ServiceControllers.getServicesByID);
-router.route("/").post(ServiceControllers.createNewService);
-router.route("/:id").patch(ServiceControllers.updateNewService);
+router.route("/").get(ServiceControllers.getServices); //Will only be used by the Client Front End
+router.route("/:currentStoreID").get(ServiceControllers.getServicesByStore); //Used in Both Front End to get all the services for a store
+router.route("/modif/:serviceID").get(ServiceControllers.getServicesByID); //Used in Both Front End to get all the info about 1 service
+router.route("/").post(ServiceControllers.createNewService); //Used only by the Store Front End to Create a Service
+router.route("/:id").patch(ServiceControllers.updateNewService); //Used only by the Store Front End to Update a Service
 
 export default router;
--- src/types/ServiceType.ts
+++ src/types/ServiceType.ts
@@ -1,13 +1,28 @@
 import mongoose, { Date } from "mongoose";
 
+export type AppointementMethod = "online" | "phone" | "none";
+export type AppointementCategorie =
+  | "Santé de la Bouche"
+  | "Santé de la Femme"
+  | "Santé de la Peau"
+  | "Soin des Yeux"
+  | "Santé Digestive"
+  | "Santé Voyage"
+  | "Santé des Oreilles"
+  | "Soin des Pieds"
+  | "Cholestérol"
+  | "Diabète"
+  | "Hypertension"
+  | "Injection et Prélevements";
+
 type ServiceType = {
   _id: mongoose.Types.ObjectId;
   name: string;
   description: string;
   price: number;
   duration: number;
-  appointementMethod: "online" | "phone" | "none";
-  appointementCategorie: " ";
+  appointementMethod: AppointementMethod;
+  appointementCategorie: AppointementCategorie;
   baseService: boolean;
   storeID?: string[];
   id: number;
@@ -15,15 +30,15 @@ type ServiceType = {
   updatedAt: Date;
   __v: number;
 };
-
+//Get Service by Store Request Param Type
 export type ReqStoreParamType = {
   currentStoreID: string;
 };
-
+//Get Service by Id Request Param Type
 export type ReqServParamType = {
   serviceID: string;
 };
-
+//Create Service Request Body Type
 export type CreateReqBodyType = {
   name: string;
   description: string;
@@ -32,7 +47,7 @@ export type CreateReqBodyType = {
   appointementMethod: string;
   appointementCategorie: string;
 };
-
+//Update Service Request Body Type
 export type UpdateReqBodyType = {
   id: number;
   name: string;
